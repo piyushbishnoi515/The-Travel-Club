f7a14ed9d34231971fd45cc4c97ce971e985c3e2
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-06-20 15:04:19 +0530

    slider with responsive done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,31 @@ function openNav() {
     document.body.classList.toggle("overflow_hidden");
     document.querySelector(".menu_1").classList.toggle("cross")
 }
+$(".parentslider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+            }
+        }
+    ]
+});
