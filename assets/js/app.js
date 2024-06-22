2cd50197730a6fa8a5e0d6b8bf69bccedc5ba332
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-06-22 11:27:21 +0530

    accodion static done

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,14 @@ $(".parentslider").slick({
         }
     ]
 });
+const accordian = document.querySelectorAll(".Accordian");
+
+accordian.forEach(e => {
+    e.addEventListener("click", function () {
+        const acc = document.querySelector(".Accordian.active");
+        e.classList.toggle("active");
+        acc && acc.classList.remove("active");
+    })
+
+})
+
